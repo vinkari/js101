3bfe5fe9f7a3e072aa50753c6516ac9b82b84708
Vinod Karivelil <vinodkarivelil@gmail.com>
2022-05-04 12:07:45 -0300

    changed file names

--- small_problems/easy_6/7_name_swapping.js
+++ small_problems/easy_6/7_name_swapping.js
@@ -1,5 +1,9 @@
 /*
 Write a function that takes a string argument consisting of a first name, a space, and a last name, and returns a new string consisting of the last name, a comma, a space, and the first name.
+
+Further Exploration
+What if the person has one or more middle names? 
+Refactor the current solution so that it can accommodate this; the middle names should be listed after the first name.
 */
 
 function swapName(name) {
